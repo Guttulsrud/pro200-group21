faa1d47ffdd1c6f3460f85c9be0f08544292590e
HADGHUB <guttulsrud@outlook.com>
2019-12-10 15:02:44 +0100

    Merge with feature-database

--- viavy/app.js
+++ viavy/app.js
@@ -23,7 +23,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
