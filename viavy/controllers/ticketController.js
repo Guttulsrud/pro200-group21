bb4365b4687e79e7c4d8ea94d3605aba354daeab
HADGHUB <guttulsrud@outlook.com>
2019-12-10 11:08:39 +0100

    Implemented controllers

--- viavy/controllers/ticketController.js
+++ viavy/controllers/ticketController.js
@@ -1,7 +1,7 @@
 const Ticket = require('../models/ticket');
 
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED yet homepage');
+    res.send('NOT IMPLEMENTED yet kek!');
 };
 
 // Display list of all tickets.
--- viavy/routes/catalog.js
+++ viavy/routes/catalog.js
@@ -2,94 +2,94 @@ const express = require('express');
 const router = express.Router();
 
 // Require controller modules.
-const ticket_controller = require('../controllers/ticketController');
-// const user_controller = require('../controllers/userController');
-const ticketInstance_controller = require('../controllers/ticketInstanceController');
+const ticketController = require('../controllers/ticketController');
+const userController = require('../controllers/userController');
+const ticketInstanceController = require('../controllers/ticketInstanceController');
 
 
 
-/// Ticket Routes ///
+/// TICKET ROUTES ///
 
 // GET catalog home page.
-router.get('/', ticket_controller.index);
+router.get('/', ticketController.index);
 
 // GET request for creating a Ticket. NOTE This must come before routes that display Ticket (uses id).
-router.get('/ticket/create', ticket_controller.ticket_create_get);
+router.get('/ticket/create', ticketController.ticket_create_get);
 
 // POST request for creating Ticket.
-router.post('/ticket/create', ticket_controller.ticket_create_post);
+router.post('/ticket/create', ticketController.ticket_create_post);
 
 // GET request to delete Ticket.
-router.get('/ticket/:id/delete', ticket_controller.ticket_delete_get);
+router.get('/ticket/:id/delete', ticketController.ticket_delete_get);
 
 // POST request to delete Ticket.
-router.post('/ticket/:id/delete', ticket_controller.ticket_delete_post);
+router.post('/ticket/:id/delete', ticketController.ticket_delete_post);
 
 // GET request to update Ticket.
-router.get('/ticket/:id/update', ticket_controller.ticket_update_get);
+router.get('/ticket/:id/update', ticketController.ticket_update_get);
 
 // POST request to update Ticket.
-router.post('/ticket/:id/update', ticket_controller.ticket_update_post);
+router.post('/ticket/:id/update', ticketController.ticket_update_post);
 
 // GET request for one Ticket.
-router.get('/ticket/:id', ticket_controller.ticket_detail);
+router.get('/ticket/:id', ticketController.ticket_detail);
 
 // GET request for list of all Ticket items.
-router.get('/tickets', ticket_controller.ticket_list);
+router.get('/tickets', ticketController.ticket_list);
 
 
-//
-// /// User Routes///
-//
-// // GET request for creating User. NOTE This must come before route for id (i.e. display User).
-// router.get('/user/create', user_controller.user_create_get());
-//
-// // POST request for creating User.
-// router.post('/user/create', user_controller.user_create_post());
-//
-// // GET request to delete User.
-// router.get('/user/:id/delete', user_controller.user_delete_get());
-//
-// // POST request to delete User.
-// router.post('/user/:id/delete', user_controller.user_delete_post());
-//
-// // GET request to update User.
-// router.get('/user/:id/update', user_controller.user_update_get());
-//
-// // POST request to update User.
-// router.post('/user/:id/update', user_controller.user_update_post());
-//
-// // GET request for one User.
-// router.get('/user/:id', user_controller.user_detail());
-//
-// // GET request for list of all Users.
-// router.get('/users', user_controller.user_list());
+/// USER ROUTES ///
+
+// GET request for creating User. NOTE This must come before route for id (i.e. display user).
+router.get('/user/create', userController.user_create_get);
+
+// POST request for creating User.
+router.post('/user/create', userController.user_create_post);
+
+// GET request to delete User.
+router.get('/user/:id/delete', userController.user_delete_get);
+
+// POST request to delete User.
+router.post('/user/:id/delete', userController.user_delete_post);
+
+// GET request to update User.
+router.get('/user/:id/update', userController.user_update_get);
+
+// POST request to update User.
+router.post('/user/:id/update', userController.user_update_post);
+
+// GET request for one User.
+router.get('/user/:id', userController.user_detail);
+
+// GET request for list of all Users.
+router.get('/users', userController.user_list);
 
 
-/// TicketInstance routes ///
-//
-// // GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
-// router.get('/ticketinstance/create', ticketInstance_controller.ticketInstance_create_get());
-//
-// // POST request for creating BookInstance.
-// router.post('/ticketinstance/create', ticketInstance_controller.ticketInstance_create_post());
-//
-// // GET request to delete BookInstance.
-// router.get('/ticketinstance/:id/delete', ticketInstance_controller.ticketInstance_delete_get());
-//
-// // POST request to delete BookInstance.
-// router.post('/ticketinstance/:id/delete', ticketInstance_controller.ticketInstance_delete_post());
-//
-// // GET request to update BookInstance.
-// router.get('/ticketinstance/:id/update', ticketInstance_controller.ticketInstance_update_get());
-//
-// // POST request to update BookInstance.
-// router.post('/ticketinstance/:id/update', ticketInstance_controller.ticketInstance_update_post());
-//
-// // GET request for one BookInstance.
-// router.get('/ticketinstance/:id', ticketInstance_controller.ticketInstance_detail());
-//
-// // GET request for list of all BookInstance.
-// router.get('/ticketinstances', ticketInstance_controller.ticketInstance_list());
+/// TICKETINSTANCE ROUTES ///
+
+// GET request for creating a TicketInstance. NOTE This must come before route that displays TicketInstance (uses id).
+router.get('/ticketinstance/create', ticketInstanceController.ticketInstance_create_get);
+
+// POST request for creating TicketInstance.
+router.post('/ticketinstance/create', ticketInstanceController.ticketInstance_create_post);
+
+// GET request to delete TicketInstance.
+router.get('/ticketinstance/:id/delete', ticketInstanceController.ticketInstance_delete_get);
+
+// POST request to delete TicketInstance.
+router.post('/ticketinstance/:id/delete', ticketInstanceController.ticketInstance_delete_post);
+
+// GET request to update TicketInstance.
+router.get('/ticketinstance/:id/update', ticketInstanceController.ticketInstance_update_get);
+
+// POST request to update TicketInstance.
+router.post('/ticketinstance/:id/update', ticketInstanceController.ticketInstance_update_post);
+
+// GET request for one TicketInstance.
+router.get('/ticketinstance/:id', ticketInstanceController.ticketInstance_detail);
+
+// GET request for list of all TicketInstance.
+router.get('/ticketinstances', ticketInstanceController.ticketInstance_list);
+
 
 module.exports = router;
