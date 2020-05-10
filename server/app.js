a9e090754370625fa0da324994d708e373ef4c5d
Guttulsrud <guttulsrud@outlook.com>
2020-05-10 22:49:12 +0200

    Added ticketview

--- server/app.js
+++ server/app.js
@@ -34,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/main', mainRouter);
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
