aecc9d02e0bb7179855dc234d3bae9c05e3ad73c
Eirik Andreas <eirikab@gmail.com>
2020-05-14 13:47:36 +0200

    Added BottomMenuItem component, Added BottomMenuItem util, Svg Folder structure

---
+++ front-end/src/utils/BottomMenuItems.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ProfileIcon } from '../components/Svg/ProfileIcon';
+
+export const items = [
+    {
+        title: 'Ny reise',
+        icon: 'Icon1',
+        link: '#'
+    },
+    {
+        title: 'Sanntidskart',
+        icon: 'Icon2',
+        link: '#'
+    },
+    {
+        title: 'Bilett',
+        icon: 'Icon3',
+        link: '#'
+    },
+    {
+        title: 'Profil',
+        icon: <ProfileIcon />,
+        link: '#'
+    }
+];
