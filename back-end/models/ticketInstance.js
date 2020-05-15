41a817688fe85003e84feffdea0e9f1a7879ce62
hallahallan <martin.hallan@gmail.com>
2020-05-15 10:23:40 +0200

    Added archiveTicketInstance

--- back-end/models/ticketInstance.js
+++ back-end/models/ticketInstance.js
@@ -5,11 +5,12 @@ const Schema = mongoose.Schema;
 const TicketInstanceSchema = new Schema(
     {
         id: Schema.Types.ObjectID,
-        user_id: { type: Schema.Types.ObjectID , required: true},
-        ticket_id: { type: Schema.Types.ObjectID , required: true},
+        user_id: {type: Schema.Types.ObjectID, required: true},
+        ticket_id: {type: Schema.Types.ObjectID, required: true},
         number_of_tickets: {type: Number, default: 1},
         expiration: {type: Date, default: Date.now()},
-        cost: {type: Number}
+        cost: {type: Number},
+        archived: {type: Boolean, default: false}
     }
 );
 
