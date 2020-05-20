442920a916491e332c11630a6ca7d75f681f2554
Eirik Andreas <eirikab@gmail.com>
2020-05-20 18:17:37 +0200

    Added Flexbox adjustments

--- front-end/src/pages/PurchasePage.jsx
+++ front-end/src/pages/PurchasePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Div } from '../elements/divs/Div';
 import { RoundBtn } from '../elements/buttons/RoundBtn';
 import { Button } from '../elements/buttons/Button';
+import { ClockIcon } from '../components/Icons/ClockIcon'
 
 
 class PurchasePage extends React.Component {
 
     state = {
         busName: 'Vyvian',
+        arrivalTime: 3,
         count: 0,
         sum: 0,
         ticketType: [
@@ -24,10 +26,10 @@ class PurchasePage extends React.Component {
             <ul className="ticket-types">
                 {this.state.ticketType.map((ticketType, i) => (
                     <li key={i}>
-                        <h2>{ticketType.name}</h2>
-                        <Div>
+                        <h2 className="title">{ticketType.name}</h2>
+                        <Div display="flex" flexDirection="center" alignItems="center">
                             {ticketType.qty === 0 ? <RoundBtn inactive>-</RoundBtn> : <RoundBtn> - </RoundBtn>}
-                            {ticketType.qty}
+                            <Div mx="20px"><h3>{ticketType.qty}</h3></Div>
                             <RoundBtn onClick={this.handleQty()}>+</RoundBtn>
                         </Div>
                     </li>
@@ -43,16 +45,25 @@ class PurchasePage extends React.Component {
     render() {
 
         return (
-            <Div>
-                <h1>{this.state.busName}</h1>
+            <Div display="flex" flexDirection="column" mx="20px">
+                <Div display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
+                    <h1 className="display-1">{this.state.busName}</h1>
+                    <Div display="flex" alignItems="center">
+                        <ClockIcon fill="#D7D8D9" />
+                        <p className="subtitle" style={{ paddingLeft: 10 }}>Ankommer om {this.state.arrivalTime} minutter</p>
+                    </Div>
+
+                </Div>
 
                 {this.renderTicketTypes()}
-
-                <h2>Totalsum: {this.state.sum}</h2>
+                <Div display="flex" justifyContent="space-between">
+                    <h2 className="title">Totalsum</h2>
+                    <h2 className="title">{this.state.sum} kr</h2>
+                </Div>
 
                 <Button>Kjøp Billett</Button>
 
-            </Div>
+            </Div >
         );
     }
 };
