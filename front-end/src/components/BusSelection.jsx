899a9528c1cc3b1652d1c851003b27b93182f6b3
Petlas88 <lasse.pettersen88@gmail.com>
2020-05-25 14:38:04 +0200

    Continued work on bus list

--- front-end/src/components/BusSelection.jsx
+++ front-end/src/components/BusSelection.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import {Div} from '../elements/divs/Div';
 import Heading from '../elements/text/StyledHeading';
 import Text from '../elements/text/StyledText';
+import {ChevronRightIcon} from './Icons/ChevronRightIcon';
 
 
 const BusSelection = props => {
     return (
-        <Div bg={'#fff'} height={135} width={1} display={'flex'} flexDirection={'column'} color={'vyBlack'} >
+        <Div bg={'#fff'} height={135} width={1} display={'flex'} flexDirection={'column'} color={'#616567'} >
             <Div display={'flex'} justifyContent={"space-between"} pt={20} pb={30} width={1} borderBottom={"1px solid #D7D8D9"} >
                 <Div display={"flex"} pl={20}>
                     <Div height={48} width={48}>
                         <img src={'/images/bus-48.png'}/>
                     </Div>
-                    <Heading.h1 pl={10} my={0} pt={5}>Vyvian 521</Heading.h1>
+                    <Heading.h1 color={"vyBlack"} pl={10} my={0} pt={5}>Vyvian 521</Heading.h1>
                 </Div>
-                <Text.p color={"#616567"} pr={20}>4 min</Text.p>
+                <Text.p pr={20}>4 min</Text.p>
+            </Div>
+            <Div display={"flex"} m={"auto"} width={1}>
+                <Div pl={18}>
+                    <ChevronRightIcon/>
+                </Div>
+                <Text.p m={0} pt={3}>Biermannsgate</Text.p>
             </Div>
         </Div>
     );
